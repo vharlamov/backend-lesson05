0d1507da74bbdc8ea40155c90cd479d0b409a513
Valeriy Harlamov <valera545@gmail.com>
2022-02-10 19:28:41 +0300

    Lesson06 hw1

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ app.use('/api', routes) // Middleware для роутинга
 
 // app.use(cors())
 // app.options('*', cors())
-
-app.all('*', function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*')
-	res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-	next()
-})
+//
+// app.all('*', function (req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', '*')
+// 	res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+// 	next()
+// })
 
 // if (process.env.NODE_ENV === 'production') {
 // 	console.log('Production')
--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose')
 
 const schema = new Schema(
 	{
-		name: { type: String, required: true },
+		name: { type: String },
 		email: { type: String, required: true, unique: true },
 		password: { type: String, required: true },
 		completedMeetings: Number,
