1208e60861c998063c444af3afc6f6ad39219622
Valeriy Harlamov <valera545@gmail.com>
2022-02-14 13:18:46 +0300

    CORS debug

--- app.js
+++ app.js
@@ -28,10 +28,11 @@ app.use(
 // 	})
 // )
 
-app.all('*', function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*')
-	next()
-})
+// app.all('*', function (req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', '*')
+// 	res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+// 	next()
+// })
 
 // if (process.env.NODE_ENV === 'production') {
 // 	console.log('Production')
